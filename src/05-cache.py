26c00d6b9004bf89979c773a37d130a9a6b73bd3
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-02 12:16:24 -0500

    upload editor and giti

--- src/05-cache.py
+++ src/05-cache.py
@@ -1,6 +1,6 @@
 import time
 from functools import lru_cache
-'''
+
 @lru_cache(maxsize=12)
 def funcion_compleja(numero1, numero2):
     time.sleep(5)
@@ -15,12 +15,12 @@ resultado = funcion_compleja(2,2)
 print(f'El resultado es: {resultado}')
 resultado = funcion_compleja(2,2)
 print(f'El resultado es: {resultado}')
-'''
 
-def funtion(a,b):
+'''def funtion(a,b):
     result = a + b
     return result
 
 print(f'El resultado es =>',funtion(2,2))
+'''
 
 
