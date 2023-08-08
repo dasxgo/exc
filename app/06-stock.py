0890acb8de9de91fd7a841fda31fbee7d690c2dc
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-08 10:56:08 -0500

    update

--- app/06-stock.py
+++ app/06-stock.py
@@ -1,40 +1,40 @@
-class Inventario:
+class Inventory:
     def __init__(self):
-        self.articulos = {}
+        self.items = {}
 
-    def agregar_articulos(self, id_articulo, nombre_articulo, cantidad_stock, precio):
-        self.articulos[id_articulo] = {'nombre_articulo': nombre_articulo,
-                                       'cantidad_stock': cantidad_stock,
-                                       'precio': precio}
+    def add_items(self, id_item, name_product, stock_quantity, price):
+        self.items[id_item] = {'name_product': name_product,
+                                       'quantity': stock_quantity,
+                                       'price': price}
 
-    def actualizar_articulo(self, id_articulo, nombre_articulo, cantidad_stock, precio):
-        if id_articulo in self.articulos:
-            self.articulos[id_articulo] = {'nombre_articulo': nombre_articulo,
-                                           'cantidad_stock': cantidad_stock,
-                                           'precio': precio}
+    def update_items(self, id_item, name_product, stock_quantity, price):
+        if id_item in self.items:
+            self.items[id_item] = {'name_product': name_product,
+                                           'stock_quantity': stock_quantity,
+                                           'price': price}
         else:
-            print("El artículo no se encuentra en el inventario.")
+            print("The item is not in inventory..")
 
-    def verificar_detalles_articulo(self, id_articulo):
-        return self.articulos.get(id_articulo, "Articulo no encontrado en el inventario")
+    def check_item_details(self, id_item):
+        return self.items.get(id_item, "Item not found in inventory")
 
-# Crear un objeto de la clase Inventario
-mi_inventario = Inventario()
-print(f'inventario inicial:', mi_inventario.articulos)
+# Create an object of class Inventory
+my_inventory = Inventory()
+print(f'inventario inicial:', my_inventory.items)
 print('='*120)
 
-# Agregar articulos al inventario
-mi_inventario.agregar_articulos('001', 'Manzana', 100, 0.5)
-mi_inventario.agregar_articulos('002', 'Naranja', 80, 0.3)
+# Add items to inventory
+my_inventory.add_items('001', 'Apple', 100, 0.5)
+my_inventory.add_items('002', 'Orange', 80, 0.3)
 
-print(f'Agregacion articulos:', mi_inventario.articulos)
+print(f'Add Items:', my_inventory.items)
 print('='*120)
 
-# Verificar los detalles del articulo
-print(f'Verificacion articulo:', mi_inventario.verificar_detalles_articulo('001'))
+# Check item details
+print(f'Check Item:', my_inventory.check_item_details('001'))
 print('='*120)
 
 
-# Actualizar detalles del articulo
-mi_inventario.actualizar_articulo('001', 'Manzana', 120, 0.5)
-print(f'Inventario actualizado:', mi_inventario.articulos)
+# Update item details
+my_inventory.add_items('001', 'Apple', 120, 0.5)
+print(f'Update Inventory:', my_inventory.items)
