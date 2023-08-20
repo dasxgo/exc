9c57147cc87bb342b45c0296e5da820a853369ba
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-20 08:19:12 -0500

    update

---
+++ src/__main__.py
@@ -0,0 +1,6 @@
+# Call module => from <name file> import <funtion> 
+
+from .module1 import city
+
+# Run the package => python3 -m <folder> || python3 <name file>
+
---
+++ src/module1.py
@@ -0,0 +1,13 @@
+def city(country):
+    if country == 'Colombia':
+        return 'Bogota'
+    elif country == 'Brasil':
+        return 'Sao Paulo'
+    elif country == 'Peru':
+        return 'Lima'
+    elif country == 'Ecuador':
+        return 'Quito'
+    else:
+        return 'Desconocido'
+
+print(f'The city is =>',city('Peru'))
