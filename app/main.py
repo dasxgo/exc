39d57977a7865eb17ec797e4e3d23268d11cfc85
dasxgo <dasxgo@DESKTOP-VQV03SB>
2023-08-21 07:05:55 -0500

    update

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import random
+
+def main():
+    
+    def get_random_number():
+        return random.randint(1,6)
+
+    number = get_random_number()
+    print('The number is:', number)
+
+if __name__ == '__main__':
+    main()
+
